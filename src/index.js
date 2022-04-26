f4c7d62cef945266f54ea79a45fa6162e66dcd76
Isaiah <ijandicos90@gmail.com>
2022-04-26 02:16:53 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
+ 
   // your code here
-});
+ let form = document.querySelector("form")
+ 
+ form.addEventListener('submit', (e) => {
+   e.preventDefault()
+ buildATask(e.target.querySelector('#new-task-description').value)
+  })
+})
+
+function buildATask(task){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = '  delete'
+  li.textContent = `${task}  `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
