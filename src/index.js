5be287b1a866798d47933b95feab5578cab3640b
Isaiah <ijandicos90@gmail.com>
2022-05-04 10:53:54 -0400

    Added Deliverable

--- src/index.js
+++ src/index.js
@@ -6,20 +6,43 @@ document.addEventListener("DOMContentLoaded", () => {
  form.addEventListener('submit', (e) => {
    e.preventDefault()
  buildATask(e.target.querySelector('#new-task-description').value)
+
+ form.reset()
+
   })
 })
 
 function buildATask(task){
   let li = document.createElement('li')
+  document.querySelector('#tasks').appendChild(li)
   let btn = document.createElement('button')
+  let styleSpan = document.createElement("span")
   btn.addEventListener('click', handleDelete)
   btn.textContent = '  delete'
-  li.textContent = `${task}  `
+  // add text to button to make known it's intent for the user 
+  styleSpan.textContent = `${task}  ` 
+  // make task enclosed in the span tag.for styling
+  li.appendChild(styleSpan)
   li.appendChild(btn)
-  document.querySelector('#tasks').appendChild(li)
+  const prio = document.querySelector("#priority")
+  if (prio.value === "Urgent") {
+    styleSpan.style.color = "red"
+  } else if (prio.value === "Neutral") {
+    styleSpan.style.color = "blue"
+  }
+
+  else {
+    styleSpan.style.color = "green"
+  }
+  
+  console.log(li)
+  // this console log showed the value of each option in relation to select tag id
 }
 
 function handleDelete(e){
   e.target.parentNode.remove()
 }
+// handleDelete gives button delete functionality
+
+
 
